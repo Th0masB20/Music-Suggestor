ee5feae8495c8b00d2d1805da52998972090ae6e
Thommy Baez <thommybaez93@gmail.com>
2024-06-24 22:30:52 -0400

    added: - Get a random playlist you have - get random artist - spotify suggestion for the artists

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from spotify_connection import connect_to_spotify
 from winotify import Notification
 
-from spotify_functions import get_user_playlists
+from spotify_functions import get_artist_suggestions
 
 def getNotification(): 
-    spotify = connect_to_spotify()
+    spotify = connect_to_spotify()    
+    suggested_artist:list = get_artist_suggestions(spotify)
+    msg = f"Since you have listened to {list(suggested_artist[1])[0]}, we recommend you check out {suggested_artist[0]}"
     notification = Notification(app_id='Test Message', 
                                 title='Daily spotify suggestion',
-                                msg='Check out this new music playlist', 
+                                msg=msg, 
                                 duration='short')
-    get_user_playlists(spotify)
     notification.show()
     
 if __name__ == '__main__':
--- spotify_functions.py
+++ spotify_functions.py
@@ -1,11 +1,53 @@
 from spotipy import Spotify
 import json
+import random
 
-def get_user_playlists(sp:Spotify):
-    playlist_data = sp.current_user_playlists(5)
-    file = open('text.json', 'w');
-    file.write(json.dumps(playlist_data))
-    file.close()
-    for playlists in playlist_data['items']:
-        print(playlists['name'])
+#gest your playlists
+def get_user_playlists(sp:Spotify) -> dict:
+    playlist_data:dict = sp.current_user_playlists(5)
+    return playlist_data['items']
+
+#gets all songs in a random playlist
+def get_songs_in_playlist(sp:Spotify) -> dict:
+    your_playlists:dict = get_user_playlists(sp);
+    random_playlist:dict = random.choice(your_playlists)
+    songs_in_playlist:dict = sp.playlist_items(random_playlist['id'])
+    return songs_in_playlist
     
+#from the playlist, get the artists of each song
+def get_all_artists_in_album(sp:Spotify) -> dict:
+    songs_in_playlist = get_songs_in_playlist(sp)
+    artist_dic:dict = {}
+    for songs in songs_in_playlist['items']:
+        name:str = songs['track']['album']['artists'][0]['name']
+        id:str = songs['track']['album']['artists'][0]['id']   
+        artist_dic[name] = id
+        
+    return artist_dic
+
+#gets a random artist and suggestes other artists similar to them
+def get_suggestions(sp:Spotify) -> dict: 
+    artist_in_playlist:dict = get_all_artists_in_album(sp)
+    random_artist:str = random.choice(list(artist_in_playlist))
+    url = f'https://api.spotify.com/v1/artists/{artist_in_playlist[random_artist]}/related-artists'
+    suggestions = sp._get(url)
+    suggestions['based_on'] = {random_artist:artist_in_playlist[random_artist]}
+    return suggestions
+
+def get_suggested_artists(sp:Spotify) -> list:
+    suggested_dict:dict = get_suggestions(sp)
+    suggested_artists:list = [{}, {}]
+    for artist in suggested_dict['artists']:
+        name = artist['name']
+        id = artist['id']
+        suggested_artists[0][name] = id
+    
+    suggested_artists[1]['based_on'] = suggested_dict['based_on']
+    return suggested_artists
+
+def get_artist_suggestions(sp:Spotify):
+    suggested_artists:list = get_suggested_artists(sp)
+    random_suggested_artist = [str, {str: str}] 
+    random_suggested_artist[0] = random.choice(list(suggested_artists[0]))
+    random_suggested_artist[1] = suggested_artists[1]['based_on']
+    return random_suggested_artist
