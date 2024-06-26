5f3eeb8ea23c29003e0902d483f20dd7f249897a
Thommy Baez <thommybaez93@gmail.com>
2024-06-26 12:47:03 -0400

    Changes: - finished added notification - changed winotify to win11toast:     - couldnt have a call back function in winotify
    - when clicking notification, it brings you to the recomended artist's spotify page

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from spotify_connection import connect_to_spotify
-from winotify import Notification
+import webbrowser
+from win11toast import toast
+import fontstyle
 
 from spotify_functions import get_artist_suggestions
 
+def open_web(arg, url):
+    webbrowser.open_new_tab(url)
+    
 def getNotification(): 
     spotify = connect_to_spotify()    
-    suggested_artist:list = get_artist_suggestions(spotify)
-    msg = f"Since you have listened to {list(suggested_artist[1])[0]}, we recommend you check out {suggested_artist[0]}"
-    notification = Notification(app_id='Test Message', 
-                                title='Daily spotify suggestion',
-                                msg=msg, 
-                                duration='short')
-    notification.show()
-    
+    suggested_artist:list = get_artist_suggestions(spotify)    
+    msg = f"Since you have listened to {list(suggested_artist[1])[0]}, we recommend you check out {suggested_artist[0][0]}"
+    toast('Daily Spotify Suggestions', msg, on_click= lambda arg: open_web(arg=arg, url=suggested_artist[1][1][1]))
+        
 if __name__ == '__main__':
     getNotification() 
--- spotify_functions.py
+++ spotify_functions.py
@@ -21,7 +21,8 @@ def get_all_artists_in_album(sp:Spotify) -> dict:
     for songs in songs_in_playlist['items']:
         name:str = songs['track']['album']['artists'][0]['name']
         id:str = songs['track']['album']['artists'][0]['id']   
-        artist_dic[name] = id
+        url:str = songs['track']['album']['artists'][0]['external_urls']['spotify']
+        artist_dic[name] = [id, url]
         
     return artist_dic
 
@@ -29,7 +30,7 @@ def get_all_artists_in_album(sp:Spotify) -> dict:
 def get_suggestions(sp:Spotify) -> dict: 
     artist_in_playlist:dict = get_all_artists_in_album(sp)
     random_artist:str = random.choice(list(artist_in_playlist))
-    url = f'https://api.spotify.com/v1/artists/{artist_in_playlist[random_artist]}/related-artists'
+    url = f'https://api.spotify.com/v1/artists/{artist_in_playlist[random_artist][0]}/related-artists'
     suggestions = sp._get(url)
     suggestions['based_on'] = {random_artist:artist_in_playlist[random_artist]}
     return suggestions
@@ -37,17 +38,25 @@ def get_suggestions(sp:Spotify) -> dict:
 def get_suggested_artists(sp:Spotify) -> list:
     suggested_dict:dict = get_suggestions(sp)
     suggested_artists:list = [{}, {}]
+    
     for artist in suggested_dict['artists']:
         name = artist['name']
         id = artist['id']
-        suggested_artists[0][name] = id
+        url = artist['external_urls']['spotify']
+        suggested_artists[0][name] = [id, url]
     
     suggested_artists[1]['based_on'] = suggested_dict['based_on']
     return suggested_artists
-
+    
 def get_artist_suggestions(sp:Spotify):
     suggested_artists:list = get_suggested_artists(sp)
-    random_suggested_artist = [str, {str: str}] 
-    random_suggested_artist[0] = random.choice(list(suggested_artists[0]))
-    random_suggested_artist[1] = suggested_artists[1]['based_on']
+    file = open('songs.json', 'w')
+    file.write(json.dumps(suggested_artists));
+    file.close()
+    random_suggested_artist = [list, {str: list}] 
+    random_artist = random.choice(list(suggested_artists[0]))
+    random_suggested_artist[0] = [random_artist, suggested_artists[0][random_artist]]
+    
+    based_on_artist = list(suggested_artists[1]['based_on'])[0]
+    random_suggested_artist[1] = [based_on_artist, suggested_artists[1]['based_on'][based_on_artist]]
     return random_suggested_artist
